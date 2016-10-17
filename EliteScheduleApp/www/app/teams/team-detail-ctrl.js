e0736ed2c1b4bd57ae93c8bced703232a64a7717
s00141500 <s00141500@mail.itsligo.ie>
2016-10-17 19:26:24 +0100

    updated team-detail and stadings ui using ionic grid system and typography

--- EliteScheduleApp/www/app/teams/team-detail-ctrl.js
+++ EliteScheduleApp/www/app/teams/team-detail-ctrl.js
@@ -25,42 +25,41 @@
 
 		vm.teamName = team.name;
 
-		vm.games = _.chain(data.teams)
-		.filter(isTeamInGame)
-		.map(function(item){
-			var isTeam = (item.teamId === vm.teamId ? true:false );
-			var opponentName = isTeam1 ? item.team2: item.team1;
-			var scoreDisplay = getScoreDisplay(isTeam1,item.team1Score, item.team2Score);
-			return{
-				gameId: item.Id,
-				opponent:opponentName,
-				time : item.time,
-				location: item.location,
-				locationUrl: item.locationUrl,
-				scoreDisplay: scoreDisplay,
-				homeAway : (isTeam1? "vs." : "at.")
-			};
+		vm.games = _.chain(data.games)
+                        .filter(isTeamInGame)
+                        .map(function (item) {
+                            var isTeam1 = (item.team1Id === vm.teamId ? true : false);
+                            var opponentName = isTeam1 ? item.team2 : item.team1;
+                            var scoreDisplay = getScoreDisplay(isTeam1, item.team1Score, item.team2Score);
+                            return {
+                                gameId: item.id,
+                                opponent: opponentName,
+                                time: item.time,
+                                location: item.location,
+                                locationUrl: item.locationUrl,
+                                scoreDisplay: scoreDisplay,
+                                homeAway: (isTeam1 ? "vs." : "at")
+                            };
+                        })
+                        .value();
 
-		}).value();
-
-
+		console.log(vm.game);
 
 		function isTeamInGame(item){
+            return item.team1Id === vm.teamId || item.team2Id === vm.teamId;
+        }
 
-			return item.team1Id === vm.teamId || item.team2Id === vm.teamId;
-		}
-
-		function getScoreDisplay(isTeam1,team1Score,team2Score){
-			if(team1Score && team2Score){
-				var teamScore = (isTeam1? team1Score : team2Score);
-				var opponentScore = (isTeam1? teame2Score : team1Score);
-				var winIndicator = teamScore > opponentScore ? "W: " : "L: ";
-				return winIndicator + teamScore + "-" + opponentScore;
-			}
-			else{
-				return "";
-			}
-		}
+        function getScoreDisplay(isTeam1, team1Score, team2Score) {
+            if (team1Score && team2Score) {
+                var teamScore = (isTeam1 ? team1Score : team2Score);
+                var opponentScore = (isTeam1 ? team2Score : team1Score);
+                var winIndicator = teamScore > opponentScore ? "W: " : "L: ";
+                return winIndicator + teamScore + "-" + opponentScore;
+            }
+            else {
+                return "";
+            }
+        }
 
 	};
 
